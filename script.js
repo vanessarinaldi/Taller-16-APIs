52a07d1a71c67f50665e1f904be6668caee32b88
Vanessa Rinaldi <vane.rinaldif4@gmail.com>
2024-11-01 02:52:14 -0300

    add

--- script.js
+++ script.js
@@ -1,67 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchButton = document.getElementById('search-btn');
-    const searchInput = document.getElementById('search-input');
-    const characterList = document.getElementById('character-list');
-
-    const urlAPI='https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a269a761cd7be5188d40283fe285d774&hash=7069116fb5e1c733cff55d9f93da9b18';
-   
-    fetch(urlAPI)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error en la carga de personajes');
-            }
-            return response.json();
-        })
-        .then(data => {
-            renderCharacterList(data.data.results);
-        })
-        .catch(error => {
-            characterList.innerHTML = `<p>${error.message}</p>`;
-            console.error('Error:', error);
-        });
-
-    searchButton.addEventListener('click', () => {
-        const searchTerm = searchInput.value.trim().toLowerCase();
-        if (searchTerm) {
-            fetch(urlAPI)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Error en la búsqueda de personajes');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    characterList.innerHTML = '';
-                    if (data.data.results.length > 0) {
-                        renderCharacterList(data.data.results);
-                    } else {
-                        characterList.innerHTML = '<p>No se encontraron personajes.</p>';
-                    }
-                })
-                .catch(error => {
-                    characterList.innerHTML = `<p>${error.message}</p>`;
-                    console.error('Error:', error);
-                });
-        }
-    });
-
-    function renderCharacterList(characters) {
-        characterList.innerHTML = '';
-        characters.forEach(character => {
-            characterList.innerHTML += `
-                <div class="col-md-4 col-sm-6">
-                    <div class="character card comic-card">
-                        <h3 class="card-title">${character.name}</h3>
-                        <img src="${character.thumbnail.path}/portrait_medium.${character.thumbnail.extension}" class="card-img-top" alt="${character.name}" />
+        const marvel ={
+        render:()=>{
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a269a761cd7be5188d40283fe285d774&hash=7069116fb5e1c733cff55d9f93da9b18';
+        const container = document.querySelector('#character-list');
+        let contentHTML = '';
+        fetch(urlAPI)
+        .then(res => res.json()) 
+        .then((json) =>{
+            for (const hero of json.data.results){
+                let urlHero = hero.urls[0].url;
+                 contentHTML+= `
+                 <div class="col-md-4 col-sm-6 card comic-card ">
+                    <a href="${urlHero}" target="_blank">
+                        <h3 class="card-title">${hero.name}</h3>
+                        <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}"  alt="${hero.name}" 
+                        class="img-thumbnail">
+                    </a>
+                    
                     </div>
-                </div>
+                  </div>
             `;
-        });
+            }
+            container.innerHTML=contentHTML;
+        }) 
     }
+
+    };
+    marvel.render();
+
     
-
-    function md5(string) {
-        return CryptoJS.MD5(string).toString(); 
-    }
-});
-
